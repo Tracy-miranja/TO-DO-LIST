238895c45b01532ccfdaa0181a81d27bd64db43b
Tracy-miranja <barazatracy44@gmail.com>
2023-03-01 18:02:38 +0300

    fix linter error

--- src/modules/statusupdate.js
+++ src/modules/statusupdate.js
@@ -1,10 +1,9 @@
 function markAsComplete(task) {
-    task.completed = true;
-  }
-  
-  function markAsIncomplete(task) {
-    task.completed = false;
-  }
-  
-  export { markAsComplete, markAsIncomplete };
-  
+  task.completed = true;
+}
+
+function markAsIncomplete(task) {
+  task.completed = false;
+}
+
+export { markAsComplete, markAsIncomplete };
